eb0d9e75ecd7db76f18af1b83d8c518e2f9222d9
SzymonKubica <szymo.kubica@gmail.com>
2023-10-02 23:09:58 +0200

    Added a skeleton of the common data screen.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Text, TextInput, View} from 'react-native';
 
 import {useTranslation} from 'react-i18next';
@@ -11,7 +11,7 @@ export const HomeScreen = ({navigation}: {navigation: any}) => {
     <>
       <View
         style={{
-          flex: 1,
+          flex: 0,
           alignItems: 'flex-end',
           justifyContent: 'flex-start',
           marginTop: 5,
@@ -21,25 +21,98 @@ export const HomeScreen = ({navigation}: {navigation: any}) => {
           destinationScreen={Screens.settings}
         />
       </View>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{flex: 0, alignItems: 'center', justifyContent: 'flex-start'}}>
         <Text style={{fontSize: 22, fontWeight: 'bold'}}>
           {t('translation:welcome')}
         </Text>
-        <CommonDataInput/>
+        <CommonDataInput />
         <MeasurementTypeSelector navigation={navigation} />
       </View>
     </>
   );
 };
 
+enum PipeCrossSectionType {
+  ROUND = 'ROUND',
+  SQUARE = 'SQUARE',
+}
+
+type Person = {
+  name: string;
+  surname: string;
+};
+
+type CommonMeasurementData = {
+  date: Date;
+  arrivalTime: string;
+  measurementRequestor: string;
+  emissionSource: string;
+  pipeCrossSectionType: PipeCrossSectionType;
+  staffResponsibleForMeasurement: Person[];
+  temperature: number;
+  pressure: number;
+};
+
 const CommonDataInput = () => {
+  const [date, setDate] = useState(new Date());
+  const [arrivalTime, setArrivalTime] = useState('');
+  const [measurementRequestor, setMeasurementRequestor] = useState('');
+  const [emissionSource, setEmissionSource] = useState('');
+  const [pipeCrossSectionType, setPipeCrossSectionType] = useState(null);
+  const [staffResponsibleForMeasurement, setStaffResponsibleForMeasurement] =
+    useState([]);
+  const [temperature, setTemperature] = useState(0);
+  const [pressure, setPressure] = useState(0);
+
   return (
     <View>
-      <TextInput placeholder="Data" />
-      <TextInput placeholder="Data" />
-      <TextInput placeholder="Data" />
-      <TextInput placeholder="Data" />
-      <TextInput placeholder="Data" />
+      <InputRow
+        placeholder="17:00"
+        onChangeText={setArrivalTime}
+        label="Data Przyjazdu"
+      />
+      <InputRow
+        placeholder="Jan Kowalski"
+        onChangeText={setMeasurementRequestor}
+        label="Zleceniodawca"
+      />
+      <InputRow
+        placeholder="some source"
+        onChangeText={setEmissionSource}
+        label="Źródło Emisji"
+      />
+      <InputRow
+        placeholder="20 ℃ "
+        onChangeText={text => setTemperature(parseFloat(text))}
+        label="Temperatura"
+      />
+      <InputRow
+        placeholder="1100 hPa"
+        onChangeText={text => setPressure(parseFloat(text))}
+        label="Ciśnienie atmosferyczne"
+      />
+    </View>
+  );
+};
+
+const InputRow = ({
+  label,
+  placeholder,
+  onChangeText,
+}: {
+  label: string;
+  placeholder: string;
+  onChangeText: (text: string) => void;
+}) => {
+  return (
+    <View
+      style={{
+        flex: 0,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+      }}>
+      <Text>{label}</Text>
+      <TextInput placeholder={placeholder} onChangeText={onChangeText} />
     </View>
   );
 };
@@ -49,7 +122,7 @@ const MeasurementTypeSelector = ({navigation}: {navigation: any}) => {
     <>
       <View
         style={{
-          flex: 1,
+          flex: 2,
           flexDirection: 'row',
           alignItems: 'flex-end',
           justifyContent: 'flex-end',
