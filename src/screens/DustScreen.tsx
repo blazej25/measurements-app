aedd648447c6062cd1286e5d74100c654c6ead52
blazej25 <bkubica25@gmail.com>
2023-10-03 22:21:02 +0200

    Added a skeleton implementation of the dust screen.

--- src/screens/DustScreen.tsx
+++ src/screens/DustScreen.tsx
@@ -1,47 +1,63 @@
 import React from 'react';
 import {Button, Text, View, TextInput, TouchableOpacity} from 'react-native';
-import { NavigationButton } from '../components/buttons';
-import { Screens } from '../constants';
-import { colors } from '../styles/common-styles';
+import {NavigationButton} from '../components/buttons';
+import {CommonDataSchema, Screens} from '../constants';
+import {colors, defaultGap} from '../styles/common-styles';
 import {useTranslation} from 'react-i18next';
 import {styles} from '../styles/common-styles';
+import {MenuBar} from '../components/MenuBar';
+import { t } from 'i18next';
+import { InputRow } from '../components/InputRow';
 
 export const DustScreen = ({navigation}: {navigation: any}) => {
-  const [text, onChangeText] = React.useState('Useless Text');
-  const [isVisible, setVisible] = React.useState(false);
+  const [numberOfMeasurements, setNumberOfMeasurements] = React.useState('Useless Text');
+  const [end, setEnd] = React.useState('Useless Text');
+  const [time, setTime] = React.useState('Useless Text');
+  const [volume, setVolume] = React.useState('Useless Text');
+  const [filter, setFilter] = React.useState('Useless Text');
+  const [water, setWater] = React.useState('Useless Text');
   return (
     <>
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <TextInput
-        style={{color: 'black'}}
-        onChangeText={onChangeText}
-        value={text}
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'flex-start',
+        gap: defaultGap,
+      }}>
+      <InputRow
+        onChangeText={setNumberOfMeasurements}
+        label={'Ilość pomiarów'}
       />
-      <TouchableOpacity
-        onPress={() => setVisible(!isVisible)}
-        style={styles.roundedButton1}>
-        <Text style={styles.buttonText1}>
-          Show input
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => console.log(text)}
-        style={styles.roundedButton1}>
-        <Text style={styles.buttonText1}>
-          Submit
-        </Text>
-      </TouchableOpacity>
-      {isVisible && <Text
-        style={{color: 'black'}}
-      > {text} </Text>}
-    </View>
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text> Pyły </Text>
-      <NavigationButton
-        navigation={navigation}
-        destinationScreen={Screens.home}
+      <InputRow
+        onChangeText={setEnd}
+        label={'Rodzaj końcówki'}
       />
-    </View>
+      <InputRow
+        onChangeText={setTime}
+        label={'Godzina Przyjazdu'}
+      />
+      <InputRow
+        onChangeText={setVolume}
+        label={'Objętość zaaspirowana'}
+      />
+      <InputRow
+        onChangeText={setFilter}
+        label={'Filtr'}
+      />
+      <InputRow
+        onChangeText={setWater}
+        label={'Woda'}
+      />
+      </View>
+      <MenuBar navigation={navigation} />
+      <Button title="Press me to test" onPress={() => {
+        console.log(numberOfMeasurements);
+        console.log(end);
+        console.log(time);
+        console.log(volume);
+        console.log(filter);
+        console.log(water);
+      }}></Button>
     </>
   );
 };
