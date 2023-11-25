326bb8e73b4f62ecc0762852077280e78e1ad79c
blazej25 <bkubica25@gmail.com>
2023-11-25 19:00:59 +0100

    Learning

--- src/tutorials/TutorialMainScreen.tsx
+++ src/tutorials/TutorialMainScreen.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, Text, View, TouchableOpacity} from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown';
-import { DataBar } from '../components/input-bars';
-import { colors, defaultBorderRadius, defaultGap, largeBorderRadius } from '../styles/common-styles';
+import {DataBar} from '../components/input-bars';
+import {
+  colors,
+  defaultBorderRadius,
+  defaultGap,
+  largeBorderRadius,
+} from '../styles/common-styles';
+import TogglableTextView from './TogglableTextView';
 
 // What you can see is a basic React component. The it is defined using this weird
 // arrow function syntax because web devs like to claim that they are writing
@@ -18,10 +24,13 @@ import { colors, defaultBorderRadius, defaultGap, largeBorderRadius } from '../s
 // https://www.typescriptlang.org/docs/handbook/2/objects.html and then we specify
 // the type of that object in that other object after the colon. That's why it
 // looks as if we were specifying the arguments twice.
+
 export const TutorialMainScreen = ({navigation}: any) => {
   return (
     <View>
       <Text> Hello Błażej </Text>
+      <TogglableTextView />
+      <SimpleSelector item1={"Test"} item2={"Test2"} item3={"Test3"}/>
     </View>
   );
 };
@@ -33,7 +42,41 @@ function TutorialMainScreenEquivalent({navigation}: any): JSX.Element {
       <Text> Hello Błażej </Text>
     </View>
   );
+}
+
+// Use this to define it so that the selections are an array of arbitrary length.
+// export const SimpleSelector = ({selections} : {selections: string[]}) => {
+export const SimpleSelector = ({item1, item2, item3} : {item1: string, item2: string, item3: string}) => {
+  // Add some state here using useState
+  const [selection, setSelection] = useState('');
+                                            // ^
+  //const [initialMass, setInitialMass]: [number[], Dispatch<SetStateAction<number[]>>] = useState([0, 0, 0]);
+  //const [afterMass, setAfterMass]: [number[], Dispatch<SetStateAction<number[]>>] = useState([0, 0, 0]);
+  //const [n, setN] = useState(1);
+  const selections = [item1, item2, item3];
+
+  return (
+    <View>
+        {
+        // Add a text box here showing the current selection 
+        }
+      <Text>{selection}</Text>
+
+      <SelectDropdown
+        // You also need to define the possible selections here or pass them in as props
+        data={selections}
+        // Update selection on the line below and print it to the console.
+        onSelect={(selectedItem, index) => {
+          // Use the setter of your state to update the current selection.
+          setSelection(selectedItem)
+        }}
+        buttonTextAfterSelection={(selectedItem, _index) => selectedItem}
+        rowTextForSelection={(item, _index) => item}
+      />
+    </View>
+  );
 };
+
 // This is an equivalent syntax and it works fine, for some reason the syntax
 // above is more frequently used. As you will notice it returns a JSX.Element
 // type. This type is basically this enhanced html that you write inside of the
@@ -47,13 +90,26 @@ function TutorialMainScreenEquivalent({navigation}: any): JSX.Element {
 // Below I will explain how one of the custom components works:
 // This is the component responsible for giving you a dropdown list of choices
 // on which you can click to set the selection.
-const SelectorBar = ({ // Here are the props that you can configure on the SelectorBar
-  label,          // Label of the bar in the ui e.g. 'RODZAJ PRZEWODU'
-  selections,     // an array of the selection to choose from
-  onSelect,       // custom function that you write to execute when the user makes a selection
-  selectionToText,// convertion function which given the selection array gives you a different string.
-                  // the purpose of this is that you migth want to display different text for selection depending on the language
-}: {
+function foo(): number {
+  return 0;
+}
+const MyComponent = ({label, label2}: {label: string; label2: string}) => {
+  return (
+    <View>
+      {
+        // JSX goes here
+      }
+    </View>
+  );
+};
+const SelectorBar = ({
+  // Here are the props that you can configure on the SelectorBar
+  label, // Label of the bar in the ui e.g. 'RODZAJ PRZEWODU'
+  selections, // an array of the selection to choose from
+  onSelect, // custom function that you write to execute when the user makes a selection
+  selectionToText, // convertion function which given the selection array gives you a different string.
+}: // the purpose of this is that you migth want to display different text for selection depending on the language
+{
   label: string;
   selections: string[];
   onSelect: (selectedItem: string, index: number) => void;
@@ -127,7 +183,7 @@ const SelectorBar = ({ // Here are the props that you can configure on the Selec
             rowStyle={selectorItemStyle}
             dropdownStyle={dropdownStyle}
             buttonTextStyle={{fontSize: 14}}
-            data={["Test"]}
+            data={['Test']}
             onSelect={() =>
               console.log('Dropdown selected when no selections available')
             }
@@ -137,6 +193,9 @@ const SelectorBar = ({ // Here are the props that you can configure on the Selec
     </DataBar>
   );
 };
+
+// Conditional rendering:
+// https://react.dev/learn/conditional-rendering
 
 // Summarising the overall workflow is as follows: define props of a component,
 // then when using it as a component, you can set them using that modifier syntax like
@@ -149,4 +208,3 @@ const SelectorBar = ({ // Here are the props that you can configure on the Selec
 //  data={["Test"]}
 //  onSelect={() =>
 //    console.log('Dropdown selected when no selections available')
-
