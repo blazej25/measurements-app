ac21f0c0db940f6a3fb9e1287b18424d246f5c75
SzymonKubica <szymo.kubica@gmail.com>
2024-06-08 15:50:49 +0100

    Updated the script.

--- count-lines.sh
+++ count-lines.sh
@@ -1,1 +1,2 @@
- find src | grep -E '\.ts|\.tsx' | xargs wc -l | grep total | awk '{print $1}'
+echo "Total lines of code: "
+find src | grep -E '\.ts|\.tsx' | xargs wc -l | grep total | awk '{print $1}'
